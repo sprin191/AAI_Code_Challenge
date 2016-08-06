d6804ef53b620cc51262c439f1ea258e35a55424
Hannah <hannahrspringer@gmail.com>
2016-08-06 15:01:18 -0500

    Video retrieval scope function working.

--- server/public/scripts/controllers/homeController.js
+++ server/public/scripts/controllers/homeController.js
@@ -33,7 +33,27 @@ request.send(JSON.stringify(body));
 
 }
 
-//Submits video.
+$scope.retrieveAllVideos = function() {
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://proofapi.herokuapp.com/videos?page&per_page');
+
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('X-Auth-Token', authToken);
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
+
+};
+
+//Submits a new video.
 $scope.submitVideo = function () {
 
 var slug = $scope.video.title.replace(/\s+/g, '-').toLowerCase();
